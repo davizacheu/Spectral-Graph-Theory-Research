2e3eab2bae961be6735ea0e65dd5607adc0e86a7
Davi Fernandes <davizach@login01.rc.byu.edu>
2024-06-05 10:27:59 -0600

    Updated files

--- testing_jax.py
+++ testing_jax.py
@@ -1,31 +1,23 @@
 import numpy as np
 import jax.numpy as jnp
-import time
+from time import time
 import networkx as nx
 from sage.graphs.graph import Graph
 from sage.graphs.graph_generators import graphs
 import concurrent.futures
-from multiprocessing import Pool, cpu_count, Manager
+from multiprocessing import Pool, cpu_count, Manager, Queue, Pipe
+from multiprocessing.pool import ThreadPool
 import os
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-
-def deg_matrix(G):
-    return np.diag([G.degree(v) for v in G.nodes()])
-
-def ihara_matrix(G):
-    A = nx.adjacency_matrix(G).todense()
-    D = deg_matrix(G)
-    I = np.identity(len(G.nodes()))
-    Z = np.zeros([len(G.nodes()), len(G.nodes())])
-    return np.block([[A, D - I], [-I, Z]])
-
+from collections import defaultdict 
+from sys import getsizeof
 
 def sample_code():
     #### Generate 1 million random matrices in chunks of 100k ####
     k = 30
     chunk_size = int(1e5)
-    num_chunks = 10
+    num_chunks = 90
     t = time()
     AA = []
     for ii in range(num_chunks):
@@ -56,71 +48,62 @@ def sample_code():
 
         print('chunk: ', ii, ', dt_move=', dt_move, ', dt_eig=', dt_eig)
 
-def ihara_matrix_gen(graph_gen):
-    for G in networkx_graph_gen(graph_gen):
-        yield ihara_matrix(G)
-
-def networkx_graph_gen(gen):
-    for G in gen:
-        yield G.networkx_graph()
-
-def collect_ihara_matrices(matrix_gen):
-    list = []
-    for M in matrix_gen:
-        list.append(M)
-    return list
-
-def hash_eigenvalues(M):
-    return np.linalg.eigvals(M)
-
-def parallel_processes_generator(processing_function, generator):
-    # Use ThreadPoolExecutor for parallel processing
+def submit_task_to_process_pool_exec(graph_generator):
+    result_dict= defaultdict(Graph)
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        map = executor.map(processing_function, generator)
+        futures = []
+        for G in graph_generator:
+            future = executor.submit(return_tuple_graph_and_function, G)
+            futures.append(future)
+        for f in futures:
+            tuple = f.result()
+            if tuple[0] in result_dict:
+                print('Cospectral pair:')
+                print(tuple[1].to_dictionary()) 
+                print(result_dict[tuple[0]].to_dictionary())
+            result_dict[tuple[0]] = tuple[1]
+    print('Length of list:', len(result_dict))
+    
+
+def parallel_threads_processor(process_element, generator):
+    results = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Map the generator to the executor 
+        future_to_element = {executor.submit(process_element, element): element for element in generator}
+        for future in concurrent.futures.as_completed(future_to_element):
+            element = future_to_element[future]
+            try:
+                result = future.result()
+            except Exception as exc:
+                print(f'Element {element} generated an exception: {exc}')
+            else:
+                results.append(result)
+    return results
+
+def return_tuple_graph_and_function(G):
+    return (G.ihara_zeta_function_inverse(), G)
+
+def pool_process(graph_gen):
+    pool = Pool()
+
+    list = pool.map(return_tuple_graph_and_function, graph_gen)
+    print('Length of list:',len(list))
+
+def thread_pool(graph_gen):
+    pool = ThreadPool()
+    map = pool.map(return_tuple_graph_and_function, graph_gen, chunksize=10000)
+    pool.close()
+    pool.join()
     return map
 
-def process_graph_to_ihara_matrix(G):
-    return ihara_matrix(G.networkx_graph())
-
-def populate_dict_ihara_zeta_to_graphs(graph: Graph, shared_dict):
-    zeta_function_inverse = graph.ihara_zeta_function_inverse()
-    shared_dict[zeta_function_inverse]
-
-def multiprocess_generator(processing_function, generator, chunksize):
-    with Pool() as pool:
-        list = pool.starmap(processing_function, generator, chunksize)
-    return list
 
 def map_zeta_inverse_function_to_graphs(G: Graph):
     key = G.ihara_zeta_function_inverse()
     if key not in shared_dict:
-        shared_dict[key] = manager.list()
+        shared_dict[key] = list()
     else:
         shared_list_of_keys.append(key)
     shared_dict[key].append(G)
-
-def main():
-    print("Number of cpus from cpu_count in multiprocessing module:", os.cpu_count())
-    print("Number of cpus from cpu_count in os module:", cpu_count())
-    print("Number of logical cpus the calling thread is restriced to:", len(os.sched_getaffinity(0)))
-
-    # Generate graphs
-    graph_gen = graphs.nauty_geng("10 -c -d2")
-
-    pool = Pool()
-
-    pool.map(map_zeta_inverse_function_to_graphs, graph_gen)
-
-    # Close pool.
-    pool.close()
-
-    # Wait for all thread.
-    pool.join()
-
-    print("Displaying graphs in pdf file")
-
-    # Display graphs
-    display_cospectral_graphs()
 
 def display_cospectral_graphs():
     # Create a PDF with the plots and text
@@ -145,14 +128,51 @@ def display_cospectral_graphs():
                 pdf.savefig(fig)
                 plt.close(fig)
 
-# Manager to create shared object.
-manager = Manager()
+def populate_queue_with_graph_and_function(graph : Graph, queue : Queue, conn):
+    data = queue.get()
+    if data[graph.ihara_zeta_function_inverse()] is not None :
+        conn.send([graph, data[graph.ihara_zeta_function_inverse()]])
+    data[graph.ihara_zeta_function_inverse()] = graph
+    queue.put(data)
+
+def populate_dict_with_graph_and_function(graph : Graph, shared_data, conn):
+    if graph.ihara_zeta_function_inverse() in shared_data :
+        conn.send([graph, shared_data[graph.ihara_zeta_function_inverse()]])
+    shared_data[graph.ihara_zeta_function_inverse()] = graph
+    
+
+def async_apply(graph_generator):
+    pool = Pool()
+    manager = Manager()
+    shared_dict = manager.dict()
+    parent_conn, child_conn = Pipe()
+    for G in graph_generator:
+        pool.apply_async(func=populate_dict_with_graph_and_function, args=(G, shared_dict, child_conn))
+    pool.close()
+    pool.join()
+    while parent_conn.poll():
+        input = parent_conn.recv()
+        print(input)
+
+def main():
+    print("Number of cpus from cpu_count in multiprocessing module:", os.cpu_count())
+    print("Number of cpus from cpu_count in os module:", cpu_count())
+    print("Number of logical cpus the calling thread is restriced to:", len(os.sched_getaffinity(0)))
+
+    # Generate graphs
+    graph_gen = graphs.nauty_geng("10 -c -d2")
+    t = time()
+    result = list(graph_gen)
+    print('Memory space taken:', getsizeof(result))
+    dt = time() - t
+    print('Time taken:', dt)
+
 
 # Create a global shared dictionary
-shared_dict = manager.dict()
+shared_dict = dict()
 
 # Create a global shared list of keys with multiple values
-shared_list_of_keys = manager.list()
+shared_list_of_keys = list()
 
 if __name__ == "__main__":
     main()
