8606fd64dc84633a175163d817330027f1d56b24
davizacheu <zacheudavi@gmail.com>
2025-07-21 12:32:16 -0600

    Added expand family constructor of XO graphs

--- xo_graph_helper_functions.py
+++ xo_graph_helper_functions.py
@@ -2,10 +2,13 @@ def add_edge(graph_dict, vertex_1, vertex_2):
     graph_dict[vertex_1].append(vertex_2)
     graph_dict[vertex_2].append(vertex_1)
 
-def add_X_edges_in_block_num(graph_dict, first_vertex, gap):
-    add_edge(graph_dict, first_vertex, first_vertex + 1 + gap)
-    add_edge(graph_dict, first_vertex + 1, first_vertex + gap)
+def add_complete_bipartite_edges_in_block(graph_dict, first_vertex, gap):
+    for i in range(gap):
+        for j in range(gap):
+            if i != j:
+                add_edge(graph_dict, first_vertex + i, first_vertex + gap + j)
 
 def add_simple_chain(graph_dict, start_vertex, end_vertex, step):
     for i in range(start_vertex, end_vertex, step):
         add_edge(graph_dict, i, i + step)
+
--- xo_graphs.py
+++ xo_graphs.py
@@ -3,17 +3,11 @@ from collections import deque
 from binary_arrays_for_xo_graphs import all_binary_arrays_no_mirroring_gen
 from xo_graph_helper_functions import *
     
-def xo_dicts_gen(n, isAlternating=True):
-    if isAlternating:
-        x_gap = 2
-        v_gap = 1
-        step = 2
-    else:
-        x_gap = n
-        v_gap = n
-        step = 1
+def xo_dicts_gen(n_rows, n_cols, twin_connectivity='cycle'):
+    h_step = n_rows
+    v_step = 1
 
-    for array in all_binary_arrays_no_mirroring_gen(n - 3):
+    for array in all_binary_arrays_no_mirroring_gen(n_cols - 3):
 
         # Append 1's on the extremes of the array
         array = deque(array)
@@ -25,38 +19,30 @@ def xo_dicts_gen(n, isAlternating=True):
         graph_dict_left = defaultdict(list)
         graph_dict_right = defaultdict(list)
 
-        # Add top chain
-        add_simple_chain(graph_dict_left, 0, step*(n - 1), step)
-        add_simple_chain(graph_dict_right, 0, step*(n - 1), step)
-
-        # Add bottom chain
-        add_simple_chain(graph_dict_left, v_gap, 2*n - 1, step)
-        add_simple_chain(graph_dict_right, v_gap, 2*n - 1, step)
+        # Add chain for each row
+        for r in range(n_rows):
+            add_simple_chain(graph_dict_left, r*v_step, h_step*(n_cols - 1), h_step)
+            add_simple_chain(graph_dict_right, r*v_step, h_step*(n_cols - 1), h_step)
 
         for i in range(len(array)):
             if array[i] == 1:
+                vertical_twin_group_first_node = i * h_step
+
                 # Add X
-                add_X_edges_in_block_num(graph_dict_left, i*step, x_gap)
-                add_X_edges_in_block_num(graph_dict_right, i*step, x_gap)
+                add_complete_bipartite_edges_in_block(graph_dict_left, vertical_twin_group_first_node, h_step)
+                add_complete_bipartite_edges_in_block(graph_dict_right, vertical_twin_group_first_node, h_step)
 
-                # Add vertical to the left of X
-                add_edge(graph_dict_left, i*step, i*step + v_gap)
-                # Add vertical to the right of X
-                add_edge(graph_dict_right, (i + 1)*step, (i + 1)*step + v_gap)
-
+                # Add edges among twin vertex groups
+                if twin_connectivity == 'cycle':
+                    for j in range(n_rows):
+                        add_edge(graph_dict_left, vertical_twin_group_first_node + j, vertical_twin_group_first_node + (j + v_step) % n_rows)
+                        add_edge(graph_dict_right, vertical_twin_group_first_node + h_step + j, vertical_twin_group_first_node + h_step + (j + v_step) % n_rows)
+       
         yield (graph_dict_left, graph_dict_right, array)
 
-def obtain_positions_for_xo_graph(luke_graph_dict, isAlternating=True):
-    if isAlternating:
-        step = 2
-    else:
-        step = 1
-
+def obtain_positions_for_xo_graph(n_rows, n_cols):
     positions = {}
-    for t in range(0, step*(len(luke_graph_dict) // 2), step):
-        positions[t] = (t, 1)
-    x = 0
-    for b in range(len(luke_graph_dict) - 1 - t, len(luke_graph_dict), step):
-        positions[b] = (x, 0)
-        x += step
+    for c in range(0, n_cols):
+        for r in range(0, n_rows):
+            positions[c*n_rows + r] = (c, n_rows - r)
     return positions
